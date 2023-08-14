28a1f5dd56629f3bea92bd71069ef92a7805da56
NagendraNaiduMarrapu <87158953+NagendraNaiduMarrapu@users.noreply.github.com>
2023-08-14 11:50:06 +0530

    Add files via upload

---
+++ ShoppingCart.java
@@ -0,0 +1,10 @@
+package com.vamsi;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/ShoppingCart")
+public class ShoppingCart extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+}
